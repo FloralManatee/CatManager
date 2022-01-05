9193122e9271b873b1d69a9ea4e939a7bcbeea83
Ashlee Jackson <Ashleeannjackson@gmail.com>
2022-01-06 10:37:06 +1100

    Show/Hide components
    Partially completed

---
+++ catsfront/src/components/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class test extends React.Component {
+
+    render() {
+        return (
+        <div>
+            <h1> Other Page </h1>
+        </div>
+    );}
+}
+
+export default test;
--- catsfront/src/index.js
+++ catsfront/src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Cats from './components/cats';
+import Test from './components/test';
+
+function openList() {
+  console.log("button clicked")
+  document.getElementById('listComponent').style.display = "none";
+}
 
 function App() {
+  const [showCats, setCats] = useState(false);
+  const [showTest, setTest] = useState(false);
   return(
     <div>
-      <Cats />
+      <div>
+        <button className="option" onClick={() => setCats(!showCats)}>Cats on Record</button>
+        <button className="option" onClick={() => setTest(!showTest)}>Record a new Cat</button>
+        <button className="option">Update an existing Cat</button>
+        <button className="option">Delete a Cat</button>
+      </div>
+      <div>
+        { showCats ? <Cats /> : null }
+        { showTest ? <Test /> : null }
+      </div>
     </div>
   )
 }
