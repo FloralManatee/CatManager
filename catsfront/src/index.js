188c52b7fb60cf7b9575e9fe51d6012f81351b68
Ashlee Jackson <Ashleeannjackson@gmail.com>
2022-01-06 11:40:41 +1100

    QOL changes, css page decoration

--- catsfront/src/index.js
+++ catsfront/src/index.js
@@ -2,24 +2,24 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Cats from './components/cats';
 import Test from './components/test';
-
-function openList() {
-  console.log("button clicked")
-  document.getElementById('listComponent').style.display = "none";
-}
+import './style.css';
 
 function App() {
   const [showCats, setCats] = useState(false);
   const [showTest, setTest] = useState(false);
   return(
     <div>
-      <div>
+      <div id="header">
+        <h1 id="companyName">Sparkle City Cat Adoptions</h1>
+      </div>
+      <nav id="nav">
         <button className="option" onClick={() => setCats(!showCats)}>Cats on Record</button>
+        <button className="option">Find a Cat</button>
         <button className="option" onClick={() => setTest(!showTest)}>Record a new Cat</button>
         <button className="option">Update an existing Cat</button>
         <button className="option">Delete a Cat</button>
-      </div>
-      <div>
+      </nav>
+      <div id="componentDisplay">
         { showCats ? <Cats /> : null }
         { showTest ? <Test /> : null }
       </div>
